0e9a8a7ed691efa06493396fbd0b0119c54b17c1
Sam Handler <sam@hashicorp.com>
2016-07-04 14:49:01 -0600

    make it so you can initialize a station from xml

--- lib/capital_bikeshare/station.rb
+++ lib/capital_bikeshare/station.rb
@@ -1,25 +1,62 @@
 module CapitalBikeshare
   class Station
-    XML_FIELDS = [
-      :id,
-      :name,
-      :terminalName,
-      :lastCommWithServer,
-      :lat,
-      :long,
-      :installed,
-      :locked,
-      :installDate,
-      :removalDate,
-      :temporary,
-      :public,
-      :nbBikes,
-      :nbEmptyDocks,
-      :latestUpdateTime
-    ]
+    require "date"
+
+    INTEGER_FIELDS = [:id, :nbBikes, :nbEmptyDocks, :terminalName]
+    DATETIME_FIELDS = [:lastCommWithServer,
+      :installDate, :removalDate, :latestUpdateTime]
+    BOOLEAN_FIELDS = [:installed, :locked, :temporary, :public]
+    FLOAT_FIELDS = [:lat, :long]
+    STRING_FIELDS = [:name]
+
+    XML_FIELDS =
+      INTEGER_FIELDS +
+      DATETIME_FIELDS +
+      BOOLEAN_FIELDS +
+      FLOAT_FIELDS +
+      STRING_FIELDS
 
     XML_FIELDS.each do |field|
-      self.class_eval { attr_accessor field }
+      attr_accessor Helpers.to_snake_case(field.to_s)
+    end
+
+    def initialize(station_attrs = nil)
+      case station_attrs
+      when REXML::Element
+        initialize_from_xml(station_attrs)
+      end
+    end
+
+    def initialize_from_xml(station_xml)
+      station_xml.elements.each do |attr_element|
+        coerced_value = Station.coerce_field_value(
+          attr_element.name.to_sym, attr_element.text)
+        attribute_name = Helpers.to_snake_case(attr_element.name)
+        send("#{attribute_name}=", coerced_value)
+      end
+      self
+    end
+
+    def self.coerce_field_value(field_name, field_value)
+      case
+      when INTEGER_FIELDS.include?(field_name)
+        field_value.to_i
+      when DATETIME_FIELDS.include?(field_name)
+        coerce_to_datetime(field_value)
+      when BOOLEAN_FIELDS.include?(field_name)
+        field_value == "true"
+      when FLOAT_FIELDS.include?(field_name)
+        field_value.to_f
+      when STRING_FIELDS.include?(field_name)
+        field_value
+      end
+    end
+
+    def self.coerce_to_datetime(field_value)
+      return unless field_value && field_value.to_i > 0
+      # sometimes there's more than 10 digits; need to figure out why
+      unix_timestamp = field_value.slice(0..9)
+      DateTime.strptime(unix_timestamp, "%s")
     end
   end
 end
--- test/capital_bikeshare/station_test.rb
+++ test/capital_bikeshare/station_test.rb
@@ -1,5 +1,6 @@
 require_relative "../test_helper"
 require "minitest/autorun"
+require "byebug"
 
 class TestCapitalBikeshareStation < Minitest::Test
   def setup
@@ -7,8 +8,44 @@ class TestCapitalBikeshareStation < Minitest::Test
   end
 
   def test_attr_accessors
-    ::CapitalBikeshare::Station::XML_FIELDS.each do |field|
+    accessors = ::CapitalBikeshare::Station::XML_FIELDS.map do |field|
+      Helpers.to_snake_case(field.to_s)
+    end
+
+    accessors.each do |field|
       assert_equal @station.respond_to?(field), true
+    end
+  end
+
+  def test_initialize_from_xml
+    station_xml = File.open(
+      File.expand_path(
+        File.join(
+          File.dirname(__FILE__), "../fixtures/station.xml"))).read
+
+    station_attrs = {
+      id: 1,
+      name: "Eads St & 15th St S",
+      terminal_name: 31000,
+      last_comm_with_server: ::CapitalBikeshare::Station.coerce_to_datetime("1466958956173"),
+      lat: 38.858662,
+      long: -77.053199,
+      installed: true,
+      locked: false,
+      install_date: ::CapitalBikeshare::Station.coerce_to_datetime(0),
+      removal_date: ::CapitalBikeshare::Station.coerce_to_datetime(nil),
+      temporary: false,
+      public: true,
+      nb_bikes: 3,
+      nb_empty_docks: 11,
+      latest_update_time: ::CapitalBikeshare::Station.coerce_to_datetime("1466958836383")
+    }
+
+    station_element = REXML::Document.new(station_xml).elements.first
+    station = ::CapitalBikeshare::Station.new(station_element)
+
+    station_attrs.each do |k, v|
+      assert_equal station.send(k), v
     end
   end
 end
