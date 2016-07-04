496c2783766b1fcf5e4fec724c85ad2582720ba3
Sam Handler <sam@hashicorp.com>
2016-07-04 14:49:38 -0600

    add Helpers module

--- lib/capital_bikeshare.rb
+++ lib/capital_bikeshare.rb
@@ -1,4 +1,5 @@
 module CapitalBikeshare
+  require_relative "capital_bikeshare/helpers"
   require_relative "capital_bikeshare/station"
   require_relative "capital_bikeshare/station_set"
   require_relative "capital_bikeshare/client"
