019f55e3b68c3686ee1fa88dbcbdfa7c9569268c
Sam Handler <sam@hashicorp.com>
2016-07-04 16:20:12 -0600

    rename capital_bikeshare.rb -> capital-bikeshare.rb

--- lib/capital-bikeshare.rb
+++ lib/capital-bikeshare.rb
@@ -3,8 +3,4 @@ module CapitalBikeshare
   require_relative "capital_bikeshare/station"
   require_relative "capital_bikeshare/station_set"
   require_relative "capital_bikeshare/client"
-
-  #def initialize
-  #  @client = Client.new
-  #end
 end
